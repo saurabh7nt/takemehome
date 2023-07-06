ef9d160746cb39ffa7a42d8b71ea751cf6c303b8
Saurabh Tajane <115925871+saurabh7nt@users.noreply.github.com>
2023-07-06 12:17:25 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
                 <div className="row">
                     <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
                         <div class="container-fluid">
-                            <Link class="navbar-brand" to="/">TakemeHome</Link>
+                            <Link class="navbar-brand" to="/">PleaseTakemeHome</Link>
                             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>

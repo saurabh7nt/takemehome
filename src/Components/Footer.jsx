42bd799eb4d1937794b60eccb3b45426a1c3fb6b
Saurabh Tajane <saurabhtajne07@gmail.com>
2023-07-06 22:54:24 +0530

    Make changes in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <>
-            <hr class="featurette-divider"></hr>
+            {/* <hr class="featurette-divider"></hr>
             <div className="container-fluid">
                 <div className="row">
                     <footer class="footer bg-dark text-white py-4">
@@ -11,6 +11,102 @@ export default function Footer() {
                             <p class="mb-0">©2017-2023 TakemeHome. All rights reserved.</p>
                         </div>
                     </footer>
+                </div>
+            </div> */}
+            {/* <hr class="featurette-divider"></hr>
+            <div className="container-fluid p-4 mt-4 mb-4 px-4">
+                <div className="row gx-4">
+                    <div className=" p-3 col-md-3 border border-dark  ">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam harum praesentium veritatis neque inventore. Recusandae at molestiae nobis cupiditate optio provident, tempora quia iure voluptas nisi accusamus beatae illo accusantium? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum at unde ea. Animi, cum voluptate? Ullam error doloribus quos incidunt labore illo aut non magnam a, voluptatum, perferendis ipsam odio? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad voluptatibus libero incidunt animi consectetur alias, doloremque consequuntur quas saepe quia ipsam? Maxime odit voluptatibus reprehenderit soluta accusantium. Inventore, ipsam!
+                    </div>
+                    <div className=" p-3 col-md-3 border border-dark ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam harum praesentium veritatis neque inventore. Recusandae at molestiae nobis cupiditate optio provident, tempora quia iure voluptas nisi accusamus beatae illo accusantium? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum at unde ea. Animi, cum voluptate? Ullam error doloribus quos incidunt labore illo aut non magnam a, voluptatum, perferendis ipsam odio? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad voluptatibus libero incidunt animi consectetur alias, doloremque consequuntur quas saepe quia ipsam? Maxime odit voluptatibus reprehenderit soluta accusantium. Inventore, ipsam!
+                    </div>
+                    <div className="col-md-3 border border-dark ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam harum praesentium veritatis neque inventore. Recusandae at molestiae nobis cupiditate optio provident, tempora quia iure voluptas nisi accusamus beatae illo accusantium? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum at unde ea. Animi, cum voluptate? Ullam error doloribus quos incidunt labore illo aut non magnam a, voluptatum, perferendis ipsam odio? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad voluptatibus libero incidunt animi consectetur alias, doloremque consequuntur quas saepe quia ipsam? Maxime odit voluptatibus reprehenderit soluta accusantium. Inventore, ipsam!
+                    </div>
+                    <div className="col-md-3 border border-dark ">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam harum praesentium veritatis neque inventore. Recusandae at molestiae nobis cupiditate optio provident, tempora quia iure voluptas nisi accusamus beatae illo accusantium? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum at unde ea. Animi, cum voluptate? Ullam error doloribus quos incidunt labore illo aut non magnam a, voluptatum, perferendis ipsam odio? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum ad voluptatibus libero incidunt animi consectetur alias, doloremque consequuntur quas saepe quia ipsam? Maxime odit voluptatibus reprehenderit soluta accusantium. Inventore, ipsam!
+                    </div>
+                </div>
+            </div> */}
+
+            
+
+            <div class="container-fluid bg-dark text-white mt-5 px-4">
+                <div class="row gx-5">
+                    <div class="col ">
+                        <div class="p-3  bg-dark text-white bg-light">
+                            <ul>
+                                <li className='d-flex  justify-content-center align-items-center mb-5'>
+                                    <p> What we do !</p>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <p>Experience learning </p>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Blog</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3  bg-dark text-white bg-light">
+                            <ul>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3  bg-dark text-white bg-light">
+                            <ul>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="p-3  bg-dark text-white bg-light">
+                            <ul>
+                                <li className='d-flex justify-content-center align-items-center mb-5 line-dark text-decoration-none'>
+                                    <a href="">Home</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                                <li className='d-flex justify-content-center align-items-center mb-5'>
+                                    <a href="">Home</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
